41f02ad4116fd7e615b558fc7688b38b89d19f33
a935855375 <935855375@qq.com>
2017-07-12 20:06:48 +0800

    begin

---
+++ app/src/main/java/com/fc/fan/another/module/common/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.fc.fan.another.module.common;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.fc.fan.another.utils.ConstantUtil;
+import com.fc.fan.another.utils.PreferenceUtil;
+import com.trello.rxlifecycle2.components.RxActivity;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+
+/**
+ * Created by fan on 7/12/17.
+ */
+
+public class SplashActivity extends RxActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Observable.timer(2, TimeUnit.SECONDS)
+                .compose(bindToLifecycle())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(aLong -> Goto());
+    }
+
+    private void Goto() {
+        boolean isLogin = PreferenceUtil.getBoolean(ConstantUtil.KEY, false);
+        if (isLogin)
+            startActivity(new Intent(this, MainActivity.class));
+        else
+            startActivity(new Intent(this, LoginActivity.class));
+        this.finish();
+    }
+}
